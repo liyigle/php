aba1c17e5872971c33d6ac8a68ebaa58e2c0962a
liyigle <82373047+liyigle@users.noreply.github.com>
2021-10-19 01:06:03 +0800

    Add files via upload

---
+++ cms_sale.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Export to PHP Array plugin for PHPMyAdmin
+ * @version 4.9.0.1
+ */
+
+/**
+ * Database `dbhouse82`
+ */
+
+/* `dbhouse82`.`cms_sale` */
+$cms_sale = array(
+  array('id' => '1','name' => '合众领岛 3室 2厅 2卫','thumb' => 'https://pic1.ajkimg.com/display/hj/9ad9077babb44ca182842f082187c880/240x180.jpg','lpmc' => '合众领岛','zj' => '125','dj' => '8993','mj' => '139','fl' => '0','shi' => '148','ting' => '171','chu' => '0','wei' => '176','yangtai' => '0','szlc' => '','zlc' => '','zxcd' => '0','cx' => '','fwpt' => '1','content' => '','fwjg' => '0','tel' => '13423045276','xingming' => '测试','fdname' => '','fdtel' => '','fdnote' => '','address' => '','louxing' => '0','keywords' => '','abstract' => '','author' => '','source' => '','ditie' => '','huxing' => '','esflx' => '94','valid' => '0','uid' => '1','area' => '222','jian' => '0','clicks' => '140','top' => '0','office_lx' => '0','office_jb' => '0','office_ts' => '','shops_lx' => '0','shops_hy' => '','shops_ts' => '','zjdate' => '1547481600','tujis' => '[{"href":"https:\\/\\/pic.fang0730.com\\/HouseImg\\/201903\\/2019032616421511.jpg","desc":"","link":"","source":"https:\\/\\/pic.fang0730.com\\/HouseImg\\/201903\\/2019032616421511.jpg"},{"href":"https:\\/\\/pic.fang0730.com\\/HouseImg\\/201811\\/2018110916077722.jpg","desc":"","link":"","source":"https:\\/\\/pic.fang0730.com\\/HouseImg\\/201811\\/2018110916077722.jpg"}]','srcid_' => '0','models' => 'sale','cid' => '0','job' => '0','map' => '','fid' => '0','checked' => '1','aid' => '1','usertype' => '0','enddate' => '0','refreshdate' => '1547552700','cate_circle' => '0','cate_metro' => '0','cate_line' => '0','eid' => '0','region' => '2226','lat' => '0','lng' => '0','officetype' => '0','officelevel' => '0','shoptype' => '0','shopindustry' => '','code' => '<iframe src="https://www.anjuke.com/pc/esf/vrview/?token=LQULLSWiMQnOounmA9WBRwTu6DLUefucxYksA64Sd3qSi7O%2FWZr0HmRrG1Ar2F0A&amp;app_id=10002&amp;city_id=139&amp;from=propview&amp;from=prop_view" frameborder="no" width="600" height="450" border="0" marginwidth="0" marginheight="0" scrolling="no"></iframe>','upvideo' => '','arch' => '0','deal' => '0','feature' => '0','attributes' => '','sort' => '0','issystem' => '0','identifier' => '6b0ba073beaf1c491542ad789222d342','create_time' => '1547552700','update_time' => '1575330483','is_delete' => '0','tradeal' => '0','deal_time' => '0','gradeschool' => '','middleschool' => '')
+);
